ad397aeb8f779ca9b4b9249ac4c9cf900ee5cd38
Andres Burbano <andres.burbano@globant.com>
2023-09-25 17:43:10 -0500

    Use good name in varibles and functions

---
+++ src/clean-code/02-names.ts
@@ -0,0 +1,37 @@
+(() => {
+
+    const temperaturesInCelsius = [33.6, 12.34];
+
+    const serverIp = '123.123.123.123';
+
+    const users = [{id: 1, email: 'fernando@google.com'},{ id: 2, email: 'juan@google.com' }, { id: 3, email: 'melissa@google.com' }];
+
+    const userEmails = users.map( user => user.email );
+
+    const canJump = false;
+    const canRun = true;
+    const hasItems = true;
+    const isLoading = false;
+
+    const initialTime = new Date().getTime();
+    //....
+    // 3 doritos después
+    //...
+    const finalTime = new Date().getTime() - initialTime;
+
+    function getBooks() {
+        throw new Error('Function not implemented.');
+    }
+
+    function getBooksFromUrl( url: string) {
+        throw new Error('Function not implemented.');
+    }
+
+    function calculateSquareArea( s: number ) {
+        throw new Error('Function not implemented.');
+    }
+
+    function printJob() {
+        throw new Error('Function not implemented.');
+    }
+})();
