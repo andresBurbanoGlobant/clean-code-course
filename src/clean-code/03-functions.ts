2a29a8dbb01fea0bfb74d64aa5b14769dc6445ae
Andres Burbano <andres.burbano@globant.com>
2023-09-28 17:07:33 -0500

    Refactoring functions

--- src/clean-code/03-functions.ts
+++ src/clean-code/03-functions.ts
@@ -34,4 +34,13 @@
         console.log('Create actor', birthDate);
         return true;        
     }
+
+    const getPayAmount = ({ isDead = false, isSeparated = true, isRetired = false }): number => {
+
+        if ( isDead ) return  1500; 
+        
+        if ( isSeparated ) return 2500; 
+
+        return isRetired ? 3000 : 4000;      
+    }
 })();
