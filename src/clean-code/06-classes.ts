e3d6f2be4d795400d156dd35c0aa4639239658cd
Andres Burbano <andres.burbano@globant.com>
2023-10-04 11:42:59 -0500

    Just play with classes and inheritance

---
+++ src/clean-code/06-classes.ts
@@ -0,0 +1,63 @@
+(() => {
+
+    // No aplica el pricipio de responsabilidad unica.
+    
+    type Gender = 'M'|'F';
+    
+    class Person {
+        constructor(
+            public name: string, 
+            public gender: Gender, 
+            public birthday: Date) {
+        }
+    }
+
+    const newPerson = new Person('Sara', 'F', new Date(2016, 5, 21))
+    console.log(newPerson);
+
+    class User extends Person {
+        private lastAccess: Date;
+
+        constructor(
+            public email: string,
+            public role: string,
+            name: string,
+            gender: Gender,
+            birthday: Date
+        ) {
+            super(name, gender, birthday);
+            this.lastAccess = new Date();
+        }
+
+        checkCredentials() {
+            return true;
+        }
+    }
+
+    class UserSettings extends User {
+        constructor(
+            public workingDirectory: string,
+            public lastOpenedFolder: string,
+            email: string,
+            role: string,
+            name: string,
+            gender: Gender,
+            birthday: Date
+        ) {
+            super(email, role, name, gender, birthday);
+        }
+    }
+
+    const userSettings = new UserSettings(
+        '/usr/home',
+        '/home',
+        'pipe12@google.com',
+        'admin',
+        'Andres',
+        'M',
+        new Date(1981, 5, 12)
+    )
+
+    console.log({ userSettings })
+
+})();
